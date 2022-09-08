1966eb9d242a176b19d29dfa9e0ec1424b53209f
unknown <diegofbisquerra@gmail.com>
2022-09-08 17:19:35 -0300

    Homework Done

--- FT-M1-master/EjerciciosExtras/homework/Repaso-M1.js
+++ FT-M1-master/EjerciciosExtras/homework/Repaso-M1.js
@@ -14,47 +14,73 @@ const {
 // Pista: utilizar el método Array.isArray() para determinar si algun elemento de array es un array anidado
 // [Para más información del método: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/isArray]
 
-var countArray = function(array) {
+var countArray = function (array) {
     // Tu código aca:
-    
+    let sum = 0
+
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            sum += countArray(array[i])
+        } else {
+            sum += array[i]
+        }
+    }
+    return sum;
 }
 
+    // Implementar la función countProps: a partir de un objeto en el cual cada propiedad puede contener
+    // cualquier tipo de dato, determinar la cantidad de propiedades de objetos en cualquier nivel, ya sea el inicial
+    // u objetos anidados
+    // Ejemplo:
+    // var obj = {
+    //   a: {
+    //     a1: 10,
+    //     a2: 'Franco',
+    //     a3: {f: 'r', a: 'n', c: {o: true}}
+    //   },
+    //   b: 2,
+    //   c: [1, {a: 1}, 'Franco']
+    // }
+    // countProps(obj)--> Deberia devolver 10 ya que el objeto inicial tiene 3 propiedades, pero a su vez
+    // dentro de a tenemos 3 propiedades mas, luego a3 tiene otras 3 y por ultimo c tiene una extra.
+    // Propiedades: a, a1, a2, a3, f, a, c, o, b, c --> 10 en total
 
-// Implementar la función countProps: a partir de un objeto en el cual cada propiedad puede contener
-// cualquier tipo de dato, determinar la cantidad de propiedades de objetos en cualquier nivel, ya sea el inicial
-// u objetos anidados
-// Ejemplo:
-// var obj = {
-//   a: {
-//     a1: 10,
-//     a2: 'Franco',
-//     a3: {f: 'r', a: 'n', c: {o: true}}
-//   },
-//   b: 2,
-//   c: [1, {a: 1}, 'Franco']
-// }
-// countProps(obj)--> Deberia devolver 10 ya que el objeto inicial tiene 3 propiedades, pero a su vez
-// dentro de a tenemos 3 propiedades mas, luego a3 tiene otras 3 y por ultimo c tiene una extra.
-// Propiedades: a, a1, a2, a3, f, a, c, o, b, c --> 10 en total
+    var countProps = function (obj) {
+        // Tu código aca:
+        let cantidad = 0
 
-var countProps = function(obj) {
-    // Tu código aca:
-
-}
+        for (const key in obj) {
+            if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+                cantidad += countProps(obj[key])
+            }
+            cantidad++
+        }
+        return cantidad;
+    }
 
 
-// Implementar el método changeNotNumbers dentro del prototype de LinkedList que deberá cambiar
-// aquellos valores que no puedan castearse a numeros por 'Kiricocho' y devolver la cantidad de cambios que hizo
-// Aclaracion: si el valor del nodo puede castearse a número NO hay que reemplazarlo
-// Ejemplo 1:
-//    Suponiendo que la lista actual es: Head --> [1] --> ['2'] --> [false] --> ['Franco']
-//    lista.changeNotNumbers();
-//    Ahora la lista quedaría: Head --> [1] --> ['2'] --> [false] --> ['Kirikocho] y la función debería haber devuelto el valor 1
+    // Implementar el método changeNotNumbers dentro del prototype de LinkedList que deberá cambiar
+    // aquellos valores que no puedan castearse a numeros por 'Kiricocho' y devolver la cantidad de cambios que hizo
+    // Aclaracion: si el valor del nodo puede castearse a número NO hay que reemplazarlo
+    // Ejemplo 1:
+    //    Suponiendo que la lista actual es: Head --> [1] --> ['2'] --> [false] --> ['Franco']
+    //    lista.changeNotNumbers();
+    //    Ahora la lista quedaría: Head --> [1] --> ['2'] --> [false] --> ['Kirikocho] y la función debería haber devuelto el valor 1
 
-LinkedList.prototype.changeNotNumbers = function(){
-    // Tu código aca:
+    LinkedList.prototype.changeNotNumbers = function () {
+        // Tu código aca:
 
-}
+        let current = this.head
+        let contador = 0
+
+        while (current) {
+            if (Number.isNaN(Number(current.value))) {
+                current.value = "Kiricocho";
+                contador++;
+            }
+            current = current.next;
+        } return contador;
+    }
 
 
 // Implementar la función mergeQueues que a partir de dos queues recibidas por parametro
@@ -67,7 +93,16 @@ LinkedList.prototype.changeNotNumbers = function(){
 
 var mergeQueues = function(queueOne, queueTwo) {
     // Tu código aca:
+    let result = new Queue()
 
+    while (queueOne.size() || queueTwo.size()) {
+        if (queueOne.size()) {
+            result.enqueue(queueOne.dequeue())
+        }
+        if (queueTwo.size()) {
+            result.enqueue(queueTwo.dequeue())
+        }
+    } return result;
 }
 
 
@@ -82,14 +117,25 @@ var mergeQueues = function(queueOne, queueTwo) {
 
 var closureMult = function(multiplier) {
     // Tu código aca:
-
+    return function (params) {
+        return multiplier * params;
+   } 
 }
 
 // Implementar el método sum dentro del prototype de BinarySearchTree
 // que debe retornar la suma total de los valores dentro de cada nodo del arbol
 BinarySearchTree.prototype.sum = function() {
     // Tu código aca:
+    let suma = this.value
+    
+    if (this.left) 
+        suma += this.left.sum()
+    
 
+    if (this.right) 
+        suma += this.right.sum()
+    
+    return suma;
 }
 
 module.exports = {
